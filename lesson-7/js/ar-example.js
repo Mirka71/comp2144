49d6997495a7e9423c371bb6675a3b5f3bff9c04
Mirka71 <200627729@student.georgianc.on.ca>
2026-02-17 16:49:56 -0500

    Added hit-test functionality

--- lesson-7/js/ar-example.js
+++ lesson-7/js/ar-example.js
@@ -37,7 +37,7 @@ const createScene = async function() {
     // STEP 1: Create a simple box, and apply a material and a colour to it.
     const box = BABYLON.MeshBuilder.CreateBox("box", {size: 0.5}, scene);
     const boxMat = new BABYLON.StandardMaterial("boxMat", scene);
-    boxMatdiffuseColor - new BABYLON.Color3(1, 0.5, 0);
+    boxMat.diffuseColor = new BABYLON.Color3(1, 0.5, 0);
     box.material = boxMat;
     // STEP 4: Move the box so it is not at your feet
     box.position.x = 1;
@@ -64,7 +64,7 @@ const createScene = async function() {
             referenceSpaceType: "local-floor"
         },
         // STEP 2c: Meta Quest requires these to be explicitly requested
-        optionalFeatures: ["hit-test", "anchors"];
+        optionalFeatures: ["hit-test", "anchors"]
     });
     // STEP 3: Commit your code and push it to a server, then try it out with a headset - notice how the orange box is right at your feet - 0, 0, 0 is located on the floor at your feet
 
@@ -73,34 +73,40 @@ const createScene = async function() {
     ---------------------------------------------------------------------------------------------------- */
     // STEP 5: A hit-test is a standard feature in AR that permits a ray to be cast from the device (headset or phone) into the real world, and detect where it intersects with a real-world object. This enables AR apps to place objects on surfaces or walls of the real world (https://immersive-web.github.io/hit-test/). To enable hit-testing, use the enableFeature() method of the featuresManager from the base WebXR experience helper.
     // STEP 5a: Create the features manager object
+    const fm = xr.baseExperience.featuresManager;
     
     // STEP 5b: Enable the hit-test feature
-    
+    const hitTest = fm.enableFeature(BABYLON.WebXRHitTest, "latest");
 
     // STEP 6a: Create a marker to show where a hit-test has registered a surface
-     
+     const marker = BABYLON.MeshBuilder.CreateCylinder("marker", {diameter: 0.15, height: 0.01 }, scene);
 
     // STEP 6b: Initialize the Quaternion so the hit-test can control rotation in all dimensions
-    
+    marker.rotationQuaternion = new BABYLON.Quaternion();
     // STEP 6c: Make the marker invisible by default
-    
+    marker.isVisible = false;
     // STEP 6d: Colour the marker
-    
+    const markerMat = new BABYLON.StandardMaterial("markerMat", scene);
+    markerMat.diffuseColor = new BABYLON.Color3(0, 1, 0);
+    markerMat.alpha = 0.5;
+    marker.material = markerMat;
 
     // STEP 7a: Create a variable to store the latest hit-test results
-    
+    let lastHitTest;
     // STEP 7b: Add an event listener for the hit-test results
-    
+    hitTest.onHitTestResultObservable.add((results) => {
         // STEP 7c: If there is a successful hit-test, then make the marker visible
-        
-            
+        if (results.length){
+            marker.isVisible = true;
             // STEP 7d: Grab the hit-test matrix of coordinates
-            
+            lastHitTest = results[0];
             // STEP 7e: Extract what we need so that the marker is oriented properly on the detected surface
-            
+            lastHitTest.transformationMatrix.decompose(underfined, marker.rotationQuaternion, marker.position);
+            }else {
             // STEP 7f: Otherwise, marker is invisible
-        
-    
+                marker.isVisible = false;
+        }
+    });
 
     /* ANCHORS
     ---------------------------------------------------------------------------------------------------- */
